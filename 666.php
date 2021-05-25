5fde48556a834a0219cd3fbc2a9da1d7a9306b2b
unknown <2425449952@qq.com>
2021-05-25 16:07:09 +0800

    sss

---
+++ 666.php
@@ -0,0 +1,60 @@
+<?php
+// 调用 time（） 函数获取 unix 时间戳
+ /* echo time ();
+  echo "\n";
+  // 显示当前文件绝对路径
+  echo  __FILE__;
+  echo "\n";
+  // 显示当前的行号
+  echo __LINE__;
+  echo "\n";
+  function add ($num1,$num2){
+    return $num1 + $num2;
+  }
+  // 定义变量$变量名
+  $a =11;
+  $b =22;
+  $c =add($a,$b);
+  // 字符串拼接 使用
+  // 使用 单引号字符串 原样输出
+  echo "$a + $b = $c";
+  */
+  // $a = "asdfghjkl";
+  // echo substr ("$a",0,3);
+  // echo "\n";
+  // $arr = ["a","b","c"];
+  //   // 将数组 用 “-” 连接成一个字符串
+	// echo implode("-",$arr);    // a-b-c
+  // echo "\n";
+  
+  // $str = "a-b-c-d-e";
+	// $arr = explode("-",$str);
+	// print_r($arr);
+  // 检测字符长度
+  // $a = "asdfghjkl";
+  // echo strlen ("$a");
+  // 将字符翻转
+    // $a = "asdfghjkl";
+    // echo strrev ("$a");
+    // 改大小写
+    
+    // $a = "sssSsdfghjkL";
+    // echo ucfirst ("$a");
+    // 开头大写
+    // $a = "cADASDFSl";
+    // echo ucwords ("$a");
+    // 全部大写
+    // $a = "asdfghjkl";
+    // echo strtoupper ("$a");
+    // 全部小写
+    // $a = "asdADSDjkl";
+    // echo strtolower ("$a");
+    // 打乱字符
+    // $a = "asdADSDjkl";
+    // echo str_shuffle	("$a");
+    // 将字符串转换成字符
+    // $a = "a-b-c-d-e";
+    // echo str_split	("$a");
+
+
+
---
+++ array.php
@@ -0,0 +1,11 @@
+<?
+// $array ="a,b,c,d";
+// print_r($array);
+// $array = array("foo", "bar", "hallo", "world");
+// var_dump($array);
+
+$arr1 = [123,545,546,48,67,321,31,'ojj','jhg','xxd'];
+
+foreach($arr1 as $k=>$v){
+    echo '$k=' .$k. '>>> $v=' .$v."\n";
+}
